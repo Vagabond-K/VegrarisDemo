6ed983a3bf783caba6395e7533ae5b211b95e57f
Vagabond K <vagabond-k@naver.com>
2025-01-27 09:09:23 +0900

    update

---
+++ Components/GameButton.razor.js
@@ -0,0 +1,8 @@
+﻿export function setState(element, pointerId, state) {
+    if (state == true) {
+        element.setPointerCapture(pointerId);
+    }
+    else {
+        element.releasePointerCapture(pointerId);
+    }
+}
